c256d61e8b46c5d1a3e47ca5261090d70628d381
Sergey Onufrienko <sergey@onufrienko.com>
2022-04-08 23:10:11 +0200

    Implement placeBid, endAuction, cancelAuction

--- contracts/auction/assembly/index.ts
+++ contracts/auction/assembly/index.ts
@@ -5,8 +5,9 @@ import {
   PersistentVector, // data structure that wraps storage to appear like a Vector
   PersistentSet, // data structure that wraps storage to appear like a Set
   u128,
+  context,
 } from 'near-sdk-as';
-import { Auction, Lot } from './models';
+import { Auction, Bid, Lot } from './models';
 
 @nearBindgen
 export class Auctions {
@@ -24,8 +25,12 @@ export class Auctions {
     return index;
   }
 
-  getAuction(id: u32): Auction {
-    return this.auctions[id];
+  getAuction(auctionId: u32): Auction | null {
+    if (this.auctions.containsIndex(auctionId)) {
+      return this.auctions[auctionId];
+    }
+
+    return null;
   }
 
   listAuctions(): Array<Auction> {
@@ -37,15 +42,45 @@ export class Auctions {
     return result;
   }
 
-  finishAuction(): void {}
+  endAuction(auctionId: u32): void {
+    assert(this.auctions.containsIndex(auctionId), 'Auction not exists');
 
-  cancelAuction(): void {}
+    let auction = this.auctions[auctionId];
+    assert(context.sender == auction.owner, 'Only auction owner allowed');
+
+    auction.ended = true;
+    this.auctions.replace(auctionId, auction);
+  }
+
+  cancelAuction(auctionId: u32): void {
+    assert(this.auctions.containsIndex(auctionId), 'Auction not exists');
+
+    let auction = this.auctions[auctionId];
+    assert(context.sender == auction.owner, 'Only auction owner allowed');
+
+    auction.canceled = true;
+    this.auctions.replace(auctionId, auction);
+  }
 
   /**
    * Bid
    */
 
-  placeBid(): void {}
+  placeBid(auctionId: u32): void {
+    assert(this.auctions.containsIndex(auctionId), 'Auction not exists');
+
+    let auction = this.auctions[auctionId];
+    assert(!auction.canceled, 'Auction is canceled');
+    assert(!auction.ended, 'Auction is ended');
+
+    const bid = new Bid();
+    assert(u128.gt(bid.bid, auction.highestBid), 'Bid should be greater then highestBid');
+
+    auction.bids.push(bid);
+    auction.highestBid = bid.bid;
+    auction.highestBidder = bid.sender;
+    this.auctions.replace(auctionId, auction);
+  }
 
   withdraw(): void {}
 }
